ad73d14bce090e3196369297a8e5685b94fc3b91
oh-naoki <oh.naoki0819@gmail.com>
2024-01-13 17:37:29 +0900

    fix path

--- naocar_x86/naocar_x86.mk
+++ naocar_x86/naocar_x86.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/oh-naoki/car/naocar_x86/device-naocar_x86.mk)
+$(call inherit-product, device/oh-naoki/naocar/naocar_x86/device-naocar_x86.mk)
 
 PRODUCT_NAME := naocar_x86
 PRODUCT_DEVICE := naocar_x86
